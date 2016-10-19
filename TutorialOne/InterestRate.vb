5b53f370b5336c326370f8a330425d03e7d76e78
nergmada <adie9@btinternet.com>
2016-10-19 16:05:27 +0100

    Initial commit

---
+++ TutorialOne/InterestRate.vb
@@ -0,0 +1,14 @@
+﻿Module InterestRate
+    Sub Main()
+        Dim Amount As Double = Console.ReadLine()
+
+        '£134.5343232
+        Dim WithInterest As Double = Amount * 1.0134625
+        '£13453.43232
+        Dim byOneHundred As Double = WithInterest * 100
+        '£13454
+        Dim TotalByOneHundred As Integer = Math.Ceiling(byOneHundred)
+        '£134.54
+        Dim Total As Double = TotalByOneHundred / 100
+    End Sub
+End Module
--- TutorialOne/Module1.vb
+++ TutorialOne/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim x As Double = Console.ReadLine()
+        Dim y As Double = Console.ReadLine()
 
+        Dim Slabs As Integer = Math.Ceiling(x / 0.609) * Math.Ceiling(y / 0.609)
+        Dim uncutSlabs As Integer = Math.Floor(x / 0.609) * Math.Floor(y / 0.609)
+
+        Dim cutSlabs As Integer = Slabs - uncutSlabs
     End Sub
 
 End Module
